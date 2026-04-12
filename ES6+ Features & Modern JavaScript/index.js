4fd72653d1c8021f2b1eb4420a28c892fa4672f5
Dominionola <theoladejitoluwa@gmail.com>
2026-04-12 23:46:26 +0100

    Asynchronous JavaScript - Callbacks & Promises assingment

--- ES6+ Features & Modern JavaScript/index.js
+++ ES6+ Features & Modern JavaScript/index.js
@@ -1,1 +1,41 @@
 // Arrow Functions & Context - Convert traditional functions to arrow functions, practice different arrow function syntaxes with various parameter counts and return types
+
+// function getArea(width, height) {
+//   return width * height;
+// }
+
+const getArea = (width, height) => {
+  return width * height;
+};
+
+console.log(getArea(2, 2));
+
+// function isPositive(number) {
+//   return number > 0;
+// }
+
+const isPositive = (number) => {
+  return number > 0;
+};
+
+console.log(isPositive(-10));
+
+// function rollDice() {
+//   return Math.floor(Math.random() * 6) + 1;
+// }
+
+const rollDice = () => {
+  return Math.floor(Math.random() * 6) + 1;
+};
+
+console.log(rollDice());
+
+// function createCar(brand, color) {
+//   return { make: brand, paint: color };
+// }
+
+const createCar = (brand, color) => {
+  return { make: brand, paint: color };
+};
+
+console.log(createCar("Gucci", "Red"));
