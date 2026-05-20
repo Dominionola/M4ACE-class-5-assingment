ba7d4d78a9ea910326a66e007e26550076df9f8d
Dominionola <theoladejitoluwa@gmail.com>
2026-05-20 13:57:26 +0100

    delete logic for todo

--- Introduction to Express.js/routes/todo.js
+++ Introduction to Express.js/routes/todo.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 let todos = [];
-nextId = 0;
+let nextId = 0;
+
+router.use((req, res, next) => {
+  console.log(`[LOG] ${req.method} request made to ${req.originalUrl}`);
+  next();
+});
 
 router.post("/", (req, res) => {
   const newTodo = {
@@ -17,12 +22,35 @@ router.get("/", (req, res) => {
   res.json(todos);
 });
 
-router.get("/:id", (req, res) => {
-  const todoId = parseInt(req.params.id, 10);
-  const foundTodo = todos.find((t) => t.id === todoId);
-  if (!foundTodo) {
-    res.status(404).json({ error: "Todo not found" });
-  }
-  res.json(foundTodo);
-});
+router
+  .route("/:id")
+  .get((req, res) => {
+    const todoId = parseInt(req.params.id, 10);
+    const foundTodo = todos.find((t) => t.id === todoId);
+    if (!foundTodo) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+    res.json(foundTodo);
+  })
+  .put((req, res) => {
+    const todoId = parseInt(req.params.id, 10);
+    const foundTodo = todos.find((t) => t.id === todoId);
+    if (!foundTodo) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+    foundTodo.title = req.body.title;
+    res.json(foundTodo);
+  })
+  .delete((req, res) => {
+    const todoId = parseInt(req.params.id, 10);
+    const todoIndex = todos.findIndex((t) => t.id === todoId);
+
+    if (todoIndex === -1) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+
+    const deletedTodo = todos.splice(todoIndex, 1);
+    res.json({ message: "Todo deleted", deletedTodo });
+  });
+
 module.exports = router;
