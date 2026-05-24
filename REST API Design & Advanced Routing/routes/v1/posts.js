b18330348bb1b0fd022fe489870290481ed88f7a
Dominionola <theoladejitoluwa@gmail.com>
2026-05-24 15:29:11 +0100

    progress on the put endpiont

--- REST API Design & Advanced Routing/routes/v1/posts.js
+++ REST API Design & Advanced Routing/routes/v1/posts.js
@@ -22,6 +22,43 @@ router
     const newPost = { id: posts.length + 1, ...req.body };
     posts.push(newPost);
     res
-      .status(200)
+      .status(201)
       .json({ success: true, message: "New Post published", data: newPost });
   });
+
+router
+  .route(":/id")
+  .get((req, res, next) => {
+    const postId = parseInt(req.params.id, 10);
+    const foundPost = posts.find((post) => post.id === postsId);
+    if (!postId) {
+      return next(AppError("No Post with this ID", 404));
+    }
+
+    res.status(201).json({ success: true, data: postId });
+  })
+  .put((req, res, next) => {
+    const postId = parseInt(req.params.id, 10);
+    const foundPost = posts.find((post) => post.id === postId);
+    if (!postId) {
+      return next(AppError("No Post with this ID", 404));
+    }
+
+    posts[postId] = {
+      ...foundPost,
+      name: req.body.name,
+      track: req.body.track,
+      edited: true,
+    };
+
+    res
+      .status(201)
+      .json({
+        success: true,
+        message: "Post succesfully edited",
+        data: foundPost,
+      });
+  })
+    .delete((req, res, next) => {
+      
+  })
