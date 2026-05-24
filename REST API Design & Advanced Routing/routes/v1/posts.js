bd0b705752858cb3051b9dddb39610ecc0141c35
Dominionola <theoladejitoluwa@gmail.com>
2026-05-24 15:41:44 +0100

    fixed issues in the posts r

--- REST API Design & Advanced Routing/routes/v1/posts.js
+++ REST API Design & Advanced Routing/routes/v1/posts.js
@@ -27,38 +27,47 @@ router
   });
 
 router
-  .route(":/id")
+  .route("/:id")
   .get((req, res, next) => {
     const postId = parseInt(req.params.id, 10);
-    const foundPost = posts.find((post) => post.id === postsId);
-    if (!postId) {
-      return next(AppError("No Post with this ID", 404));
+    const foundPost = posts.find((post) => post.id === postId);
+    if (!foundPost) {
+      return next(new AppError("No Post with this ID", 404));
     }
 
-    res.status(201).json({ success: true, data: postId });
+    res.status(200).json({ success: true, data: foundPost });
   })
   .put((req, res, next) => {
     const postId = parseInt(req.params.id, 10);
     const foundPost = posts.find((post) => post.id === postId);
-    if (!postId) {
-      return next(AppError("No Post with this ID", 404));
+    if (!foundPost) {
+      return next(new AppError("No Post with this ID", 404));
     }
 
-    posts[postId] = {
-      ...foundPost,
-      name: req.body.name,
-      track: req.body.track,
-      edited: true,
-    };
+    foundPost.title = req.body.title;
+    foundPost.content = req.body.content;
+    foundPost.edited = true;
 
-    res
-      .status(201)
-      .json({
-        success: true,
-        message: "Post succesfully edited",
-        data: foundPost,
-      });
+    res.status(200).json({
+      success: true,
+      message: "Post succesfully edited",
+      data: foundPost,
+    });
   })
-    .delete((req, res, next) => {
-      
-  })
+  .delete((req, res, next) => {
+    const postId = parseInt(req.params.id, 10);
+    const postIndex = posts.findIndex((post) => post.id === postId);
+    if (postIndex === -1) {
+      return next(new AppError("No Post with this ID", 404));
+    }
+
+    const deletedPost = posts.splice(postIndex, 1)[0];
+
+    res.status(200).json({
+      success: true,
+      message: "Post deleted sucessfully",
+      data: deletedPost,
+    });
+  });
+
+module.exports = router;
